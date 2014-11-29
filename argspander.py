cc413514e650273f6b31bc4e24769f3d03bad2d0
Richard Lancaster <richard@rl0.co.uk>
2014-11-29 23:22:26 +0000

    Argspander module that enables object argument expanding

---
+++ argspander.py
@@ -0,0 +1,76 @@
+"""
+Module that provides argument expansion using objects.
+
+It was created to enable a sane way of entering arguments gathered from
+argparse your program, without the need to pass round a arguments object.
+"""
+#The MIT License (MIT)
+#
+# Copyright (c) 2014 Richard Lancaster
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+def expand(func):
+    """Decorator that expand's an objects attributes to fill a function's arguments
+
+    The decorated function should be passed an object whose attributes share
+    the names of the function's arguments.
+    Supports functions and methods.
+
+    Decorated function args:
+        expanded: keyword arg. True to activate object expansion,
+            else the decorated function behaves normally
+
+    Usage:
+        >>> @expand
+        ... def f(a, b, c):
+        ...     print "a: %s, b: %s, c: %s" % (a, b, c)
+        >>> f(3, 2, 1)
+        a: 3, b: 2, c: 1
+        >>> class args():  # similar to the object return by argparse
+        ...     a = 3
+        ...     b = 2
+        ...     c = 1
+        >>>  f(args(), expand=True)
+        a: 3, b: 2, c: 1
+    """
+
+    def _inner(*args, **kwargs):
+        func_varnames = func.func_code.co_varnames
+
+        if kwargs.get("expand") is True and func_varnames:
+            func_args = []
+
+            # deal with methods
+            if func_varnames[0] is "self":
+                func_args.append(args[0])
+                args = args[1:]
+                func_varnames = func_varnames[1:]
+
+            func_args.extend(
+                [getattr(args[0], varname) for varname in func_varnames]
+            )
+
+            return func(*func_args)
+
+        else:
+            return func(*args, **kwargs)
+
+    return _inner
